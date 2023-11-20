e98ace62e8e811c6469f13f54cd0dc90fbe01ec2
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2023-11-19 19:06:34 -0500

    Use `expect_length`

--- tests/testthat/test-dracula_palette.R
+++ tests/testthat/test-dracula_palette.R
@@ -1,20 +1,20 @@
 test_that("palette is correct number of colors", {
-  expect_equal(length(dracula_palette()), 7)
+  expect_length(dracula_palette(), 7)
 })
 
 test_that("palette contains right colors", {
-  expect_contains(
+  expect_setequal(
     dracula_palette(),
     c("#FF5555", "#FFB86C", "#F1FA8C", "#50FA7B", "#BD93F9", "#8BE9FD", "#FF79C6")
   )
 })
 
 test_that("dracula_palette() returns less than total number of discrete colors", {
-  expect_equal(length(dracula_palette(num_cols = 5, var_type = "discrete")), 5)
+  expect_length(dracula_palette(num_cols = 5, var_type = "discrete"), 5)
 })
 
 test_that("dracula_palette() returns more than total number of discrete colors", {
-  expect_equal(length(dracula_palette(num_cols = 10, var_type = "discrete")), 10)
+  expect_length(dracula_palette(num_cols = 10, var_type = "discrete"), 10)
 })
 
 test_that("dracula_palette() returns a continuous ramp equal to the standard palette if num_cols=7", {
@@ -25,13 +25,13 @@ test_that("dracula_palette() returns a continuous ramp equal to the standard pal
 })
 
 test_that("dracula_palette() returns less than total number of colors as a continuous ramp", {
-  expect_equal(
-    length(dracula_palette(num_cols = 5, var_type = "continuous")), 5)
+  expect_length(
+    dracula_palette(num_cols = 5, var_type = "continuous"), 5)
 })
 
 test_that("dracula_palette() can return more than total number of colors as a continuous ramp", {
-  expect_equal(
-    length(dracula_palette(num_cols = 20, var_type = "continuous")), 20)
+  expect_length(
+    dracula_palette(num_cols = 20, var_type = "continuous"), 20)
 })
 
 test_that("first value in dracula_palette() continuous ramp is first value in standard palette", {
